1be2e86d9e87da8b5ebcd300877e3d8c354da1a4
Wesley Holt <wesleyjholt@gmail.com>
2024-02-29 07:01:47 -0500

    init commit

---
+++ hiermodelutils/__init__.py
@@ -0,0 +1,10 @@
+"""
+hiermodelutils
+
+Data processing utilities for hierarchical modeling.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Wesley Holt'
+# __credits__ = 'Purdue University, School of Mechanical Engineering'
+
