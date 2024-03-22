02820425db7665491afe7a0d3730ed3af471ec32
Wesley Holt <wesleyjholt@gmail.com>
2024-03-22 06:23:52 -0400

    fix imports

--- hiermodelutils/__init__.py
+++ hiermodelutils/__init__.py
@@ -8,6 +8,6 @@ __version__ = "0.1.0"
 __author__ = 'Wesley Holt'
 # __credits__ = 'Purdue University, School of Mechanical Engineering'
 
-from ._hierarchical_dataset import *
-from ._model import *
+from ._datastructures import *
+from ._hyperparam import *
 from ._poly_basis import *
